da17d5ecd0eb983afcfc7e784ba05f24712c6444
Alassane NIANG <adniang75@gmail.com>
2021-11-18 14:26:50 +0100

    Refactor Service Interface to Common Base Interface CrudService. Closes #11

--- pet-clinic-data/src/main/java/com/alassaneniang/petclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/com/alassaneniang/petclinic/service/PetService.java
@@ -2,14 +2,5 @@ package com.alassaneniang.petclinic.service;
 
 import com.alassaneniang.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById ( Long id );
-
-    Pet save ( Pet pet );
-
-    Set<Pet> findAll ();
-
+public interface PetService extends CrudService<Pet, Long> {
 }
